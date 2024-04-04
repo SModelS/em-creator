6a574c832da591c2dfbd70c67d18918f5cd67bed
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-04-04 12:26:08 +0200

    adding compilation

--- utils/make_gambit.sh
+++ utils/make_gambit.sh
@@ -1,3 +1,4 @@
-cmake .. -DWITH_HEPMC=ON -DWITH_YODA=ON -Ditch="NeutrinoBit;Mathematica;DarkBit;PrecisionBit;FlavBit;CosmoBit"
+cmake .. -DWITH_HEPMC=ON -DWITH_YODA=ON -Ditch="NeutrinoBit;Mathematica;DarkBit;PrecisionBit;FlavBit;CosmoBit;SpecBit;ObjectivesBit;NeutrinoBit;ScannerBit;DecayBit"
+make -j 2
 
 cd ../ColliderBit/examples ; g++ -I ../../Elements/include/ -I ../../Utils/include/ -I ../../Logs/include/ -I ../../cmake/include/ -I ../../contrib/mkpath/include/ -I ../../Models/include/ -I ../../contrib/yaml-cpp-0.6.2/include/ -I ../../contrib/slhaea/include/ -I ../../Backends/include/ -I ../../contrib/pybind11/include/ `python3-config --includes` -I ../../contrib/YODA-1.9.7/include/ `pkgconf eigen3 --cflags` -I ../../SpecBit/include/ -I ../../FlavBit/include/ -I ../../DarkBit/include/ -I ../include/ -I ../../contrib/heputils/include/ -I ../../CosmoBit/include -I ../../Printers/include/ -I ../../ScannerBit/include/ solo.cpp
