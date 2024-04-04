d441e95c97552c7b2533e42cd38b0ce440ab1990
Wolfgang Waltenberger <wolfgang.waltenberger@gmail.com>
2024-04-04 17:50:27 +0200

    neutrinos

--- utils/make_gambit.sh
+++ utils/make_gambit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cmake .. -DWITH_HEPMC=ON -DWITH_YODA=ON -Ditch="NeutrinoBit;Mathematica;DarkBit;CosmoBit"
+cmake .. -DWITH_HEPMC=ON -DWITH_YODA=ON -Ditch="Mathematica;DarkBit;CosmoBit"
 
 make -j `nproc`
 make ColliderBit/examples/solo.o
